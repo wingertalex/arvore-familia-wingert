7b8e35095e774408d9957d9630766b2a53662f92
wingertalex <wingert.alex@gmail.com>
2026-07-17 09:13:00 -0300

    Redireciona visualização para árvore corrigida

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const visualizandoArvore = new URLSearchParams(window.location.search).get('view') === 'tree'
+
+if (visualizandoArvore) {
+  window.location.replace('/arvore.html')
+} else {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
