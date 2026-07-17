d0b4953f90708895a5bfffb29ba3e15ff2d317b7
wingertalex <wingert.alex@gmail.com>
2026-07-17 13:20:52 -0300

    Inclui entrada separada para o editor React Flow

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        editor: resolve(__dirname, 'editor.html'),
+      },
+    },
+  },
 })
