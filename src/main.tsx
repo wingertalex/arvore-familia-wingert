e203f0432bd1d3584b7309bd2598c730788a11b1
wingertalex <wingert.alex@gmail.com>
2026-07-17 12:17:59 -0300

    Usa cadastro com sexo e filtros familiares

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './AppSexo'
 import './styles.css'
 
 const visualizandoArvore = new URLSearchParams(window.location.search).get('view') === 'tree'
